a5849bb0b743ea6363e32e018fde08a34aafc15b
Ivan Bogomolov <qhomop@gmail.com>
2023-12-07 05:47:54 +0300

    fixes

--- API/Handlers/ClusterHandler.py
+++ API/Handlers/ClusterHandler.py
@@ -1,5 +1,5 @@
 from logging import getLogger
-import numpy as np
+from settings import basedir
 from Clustering.ClusteringTickers import *
 
 from fastapi import APIRouter
@@ -8,33 +8,17 @@ from fastapi import HTTPException
 from Exchanges.MOEX.AlgoPack import *
 
 moex = MOEX()
-
 logger = getLogger(__name__)
-
 cluster_router = APIRouter()
 
 
 @cluster_router.get("/cluster_tickers")
 async def get_clustering_tickers():
     try:
-        investor_types = 3
-        long_term = 0
-        mid_term = 1
-        short_term = 2
+        data_cluster = DataClusterization()
+        data = data_cluster.read_tickers_from_json(file_path=f"{basedir}/Clustering/cached_clustering_tickers.json")
 
-        df = pd.DataFrame(Market('stocks').obstats(date='2023-11-01'))
-        clusterised_df = data_clusterization(df, ['spread_lv10', 'spread_1mio', 'spread_lv10'], investor_types)
-        long_term_df = get_tickers_for_specific_type(clusterised_df, long_term)
-        mid_term_df = get_tickers_for_specific_type(clusterised_df, mid_term)
-        short_term_df = get_tickers_for_specific_type(clusterised_df, short_term)
-
-        response_data = {
-            "long_term": long_term_df['secid'].drop_duplicates().tolist(),
-            "mid_term": mid_term_df['secid'].drop_duplicates().tolist(),
-            "short_term": short_term_df['secid'].drop_duplicates().tolist()
-        }
-
-        return response_data
+        return data
 
     except Exception as e:
         logger.error(f"Error fetching candles data: {e}")
